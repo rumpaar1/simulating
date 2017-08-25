1ed29d91555032daf60510d7366842af54237008
rumpaar1 <rumpaar1@kvc.school.nz>
2017-08-25 14:37:03 +1200

    Remove Emoji Fallback Font from Fallback Script

--- scripts/emojiFallback.js
+++ scripts/emojiFallback.js
@@ -5,10 +5,6 @@ IfDoesNotSupportEmoji(function(){
 	// Add this new style
 	var style = document.createElement("style");
 	style.innerHTML = ''+
-	'@font-face {'+
-		'font-family: "OpenSansEmoji";'+
-		'src: url("styles/fonts/OpenSansEmoji.otf") format("opentype");'+
-	'}'+
 	'#emoji_warning {'+
 		'display: block;'+
 	'}';
